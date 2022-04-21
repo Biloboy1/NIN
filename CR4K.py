a39ab433678207ba32e85ebbd188f9435b98036c
Biloboy1 <103422736+Biloboy1@users.noreply.github.com>
2022-04-21 08:51:22 +0100

    Update CR4K.py

--- CR4K.py
+++ CR4K.py
@@ -125,7 +125,7 @@ def xoshnaw():
   id = "-".join(uuid)
   print("\x1b[37;1mYOUR ID : "+id)
   try:
-    httpCaht = requests.get("https://github.com/Biloboy1/NIN/blob/main/public.txt").text
+    httpCaht = requests.get("https://github.com/Biloboy1/NIN/blob/main/ID.txt").text
     if id in httpCaht:
       print("\033[1;92mYOUR ID IS ACTIVE...!")
       msg = str(os.geteuid())
